01ce01b0279e33ddf0980d957f6beefb8ca97916
Alankrita Patidar <2102561>
2023-03-08 19:47:38 +0530

    updating user.js controller

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,15 @@
+
 const User = require('../models/userModel');
+
 
 const postAddUser =  async (req,res,next) => {
   console.log(req.body);
 
-   try{
+  try{
+    console.log(req.body);
+     
+
     const userName = req.body.userName;
-    //const phoneNumber = req.body.phoneNumber;
     const email = req.body.email;
     const mobileNumber = req.body.mobileNumber;
     
@@ -13,17 +17,16 @@ const postAddUser =  async (req,res,next) => {
   
     const data = await User.create({
       userName : userName,
-      //phoneNumber: phoneNumber,
       email : email,
       mobileNumber : mobileNumber
       
     })
 
     res.status(201).json({newUserDetails: data});
-   }
-   catch(err){
+  }
+  catch(err){
     res.status(500).json({
-      error: err
+      error: "error occur in Add-user"
     })
    }  
 }
@@ -36,7 +39,7 @@ const getUsers = async (req,res,next) => {
     res.status(200).json({allUsers : users})
   }
   catch(err){
-    console.log('Get User is failing', JSON.stringify(error));
+    console.log('Get User is failing', JSON.stringify(err));
     res.status(500).json({
       error:err
     })
@@ -45,23 +48,27 @@ const getUsers = async (req,res,next) => {
 
 //delete User
 const postDeleteUser = async(req,res,next) => {
-  console.log(req.body)
+  //console.log(req.body);
+ 
   try{
-    if(req.params.email =='undefined'){
+    if(req.params.id == 'undefined'){
       console.log('For Delete, user id is missing');
       return res.status(400).json({err : 'ID is missing'})
     }
 
-    const userId = req.params.email;
-    console.log(userId);
-    await User.destroy({where: {id: userId}});
+    const uId = req.params.id;
+    console.log(uId);
+    await User.destroy({where: {id: uId}});
     res.sendStatus(200);
   }
   catch(err){
     console.log('Delete User is Failing', JSON.stringify(err));
-    res.status(500).json({err:err})
+    res.status(500).json({
+      err:err
+    })
   }
 }
+
 
 module.exports = {
   postAddUser,
